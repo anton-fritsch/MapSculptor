967d28aebacb3535bfbf9876386d0f90543d7cc3
Anton Fritsch <anton.fritsch@gmail.com>
2013-03-17 13:28:11 -0400

    added fractional browning motion to perlin noise generation added some better visualization for maps updated readme added package, corrected automaton and updated demo

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='MapSculptor',
+      version='0.1',
+      description='Map generation utilities for 2D tilemaps',
+      author="Anton Fritsch",
+      packages=['mapsculptor'],
+      install_requires=['numpy'],
+)
